6b289b408d0f1c9ad007226294eb0b02117111ad
Alrik Zachert <az@kreatiefer.de>
2012-12-15 16:19:52 +0100

    integrate new bsd license

--- config/module.config.php
+++ config/module.config.php
@@ -1,2 +1,10 @@
 <?php
+/**
+ * Unjudder Mail Module on top of Zendframework 2
+ *
+ * @link http://github.com/unjudder/zf2-mail for the canonical source repository
+ * @copyright Copyright (c) 2012 unjudder
+ * @license http://unjudder.com/license/new-bsd New BSD License
+ * @package Uj\Mail
+ */
 return array();
--- src/Uj/Mail/Service/Transport.php
+++ src/Uj/Mail/Service/Transport.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Unjudder Mail Module on top of Zendframework 2
+ *
+ * @link http://github.com/unjudder/zf2-mail for the canonical source repository
+ * @copyright Copyright (c) 2012 unjudder
+ * @license http://unjudder.com/license/new-bsd New BSD License
+ * @package Uj\Mail
+ */
 namespace Uj\Mail\Service;
 
 class Transport
